a86b2e4a104e3421e39cf8560f1ad8ba8e6d7f3a
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-14 15:53:13 +1100

    Fix Day3

--- day03/day03.py
+++ day03/day03.py
@@ -4,50 +4,34 @@ inFile = open("input.txt")
 compass = inFile.readline()
 print(compass)
 
+def updateCoord(index, c):
+    if c == '^':
+        index = (index[0], index[1]+1)
+    elif c == 'v':
+        index = (index[0], index[1]-1)
+    elif c == '>':
+        index = (index[0]+1, index[1])
+    else:
+        index = (index[0]-1, index[1])
+    return (index)
+
 print("\n========== Part 1 ==========")
-x, y = 0, 0
+pos = (0, 0)
 houses = [(0, 0)]
 for c in compass:
-    if c == '^':
-        y +=1
-    elif c == 'v':
-        y -=1
-    elif c == '>':
-        x +=1
-    else:
-        x -=1
-    if (x, y) not in houses:
-        houses.append((x,y))
+    pos = updateCoord(pos, c)
+    if pos not in houses:
+        houses.append(pos)
 
 print(len(houses))
 
 print("\n========== Part 1 ==========")
-x1, y1, x2, y2 = 0, 0, 0, 0
+
+pos = [(0, 0), (0, 0)] #x1, y1, x2, y2
 houses = [(0, 0)]
 for c in range(len(compass)):
-    if compass[c] == '^':
-        if c%2==0:
-            y1 +=1
-        else:
-            y2 +=1
-    elif compass[c] == 'v':
-        if c%2==0:
-            y1 -=1
-        else:
-            y2 -=1
-    elif compass[c] == '>':
-        if c%2==0:
-            x1 +=1
-        else:
-            x2 +=1
-    else:
-        if c%2==0:
-            x1 -=1
-        else:
-            x2 -=1
-    if c%2 == 0 and (x1, y1) not in houses:
-        houses.append((x1,y1))
-    elif (x2, y2) not in houses:
-        houses.append((x2,y2))
+    pos[c%2] = updateCoord(pos[c%2],compass[c])
+    if pos[c%2] not in houses:
+        houses.append(pos[c%2])
 
 print(len(houses))

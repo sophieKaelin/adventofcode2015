efd151b529941da1c52a71f49638a1479ce383f1
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2021-12-14 15:13:57 +1100

    Complete Day 1 part 1

---
+++ day01/day01.py
@@ -0,0 +1,19 @@
+print("\n========== INPUTING FILE ==========")
+
+inFile = open("input.txt")
+sequence = inFile.readline()
+
+print("\n========== Part 1 ==========")
+
+print(sequence.count('(') - sequence.count(')'))
+
+print("\n========== Part 1 ==========")
+floor = 0
+for c in range(len(sequence)):
+    if sequence[c] == ")":
+        floor -=1
+        if floor < 0:
+            print("Basement: " + str(c+1))
+            break
+    else:
+        floor +=1
